ee2035e4511073ea3e8ed26650059c2090c6f486
James Whipman <80391967+jwhipman@users.noreply.github.com>
2026-01-06 10:23:12 +0100

    Adding function with 500nm resolution parameter

---
+++ HRIMC_multiple_resolutions.py
@@ -0,0 +1,170 @@
+# %%
+import os
+import numpy as np
+from tifffile import imread, imwrite
+from scipy.ndimage import gaussian_filter
+from skimage.restoration import richardson_lucy
+from skimage.util import img_as_uint
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def RLD_HRIMC_circle(input_dir, output_dir, x0,  resolution, iterations = 7):
+    # Ensure the output directory exists
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
+    # Loop through all TIFF files in the input directory
+    for filename in os.listdir(input_dir):
+        if filename.endswith(".tiff") or filename.endswith(".tif"):
+            # Read the TIFF file
+            tiff_path = os.path.join(input_dir, filename)
+            scan_data = imread(tiff_path)
+            
+            # Normalize each layer of the TIFF and apply transformations
+            processed_layers = []
+            original_layers = []
+            for layer in range(scan_data.shape[0]):
+                layer_data = scan_data[layer, :, :]
+                
+                if resolution == 333: 
+                    #Here, we model overlapping acquisitions and annotate regions with values according to laser pass map
+                    Passes = np.array([7,6,5,8,7,
+                                       7,8,7,6,6,
+                                       7,9,8,7,8,
+                                       8,7,7,6,6,
+                                       7,6,6,5,5,
+                                       6,6,5,5,4,
+                                       4,6,4,5,3,
+                                       4,5,6,6,5,
+                                       4,5,5,4,3,
+                                       4,4,3,6,5,
+                                       4,5,5,4,4,
+                                       3,3,4,4,3,
+                                       3,2,4,3,2,
+                                       2,1,1,3,2,
+                                       2,1,1,3,3,
+                                       2,2,2,2,1,
+                                       1,1,1,3,2,
+                                       1,2,2,1,1,
+                                       2,1,1,4,3,
+                                       2,1])
+                    #Here, we summarise the proportional area of each of these passes
+                    Contributions = np.array([0.0196,0.00107,0.00107,0.0034,0.0196,
+                                               0.0196,0.0034,0.0034,0.0196,0.0196,
+                                               0.0034,0.00223,0.00223,0.00223,0.0034,
+                                               0.0034,0.0034,0.0034,0.0034,0.0034,
+                                               0.0196,0.00107,0.0196,0.00107,0.0196,
+                                               0.00107,0.00107,0.00107,0.00107,0.00107,
+                                               0.00107,0.0196,0.00107,0.0196,0.00107,
+                                               0.0196,0.0196,0.0034,0.0034,0.0196,
+                                               0.0196,0.0034,0.0034,0.0196,0.0196,
+                                               0.0034,0.0034,0.0196,0.00223,0.00223,
+                                               0.00223,0.0034,0.0034,0.0034,0.0034,
+                                               0.0034,0.0034,0.0196,0.00107,0.0196,
+                                               0.00107,0.0196,0.00107,0.00107,0.00107,
+                                               0.00107,0.00107,0.00107,0.0196,0.00107,
+                                               0.0196,0.00107,0.0196,0.0034,0.0034,
+                                               0.0196,0.0196,0.0034,0.0034,0.0196,
+                                               0.0196,0.0034,0.0034,0.00223,0.00223,
+                                               0.00223,0.0034,0.0034,0.0034,0.0034,
+                                               0.00107,0.0196,0.00107,0.00219,0.00219,
+                                               0.00219,0.00219])
+
+                    Contributions = Contributions / Contributions.sum()
+                    I0 = np.max(layer_data)
+                    Passes = I0 - I0/(1+ np.exp(-(Passes - x0)))
+                    y_array = Passes*Contributions
+                    total_sum = np.sum(y_array)
+
+                    result = list((
+                        (y_array[3] + y_array[4] + y_array[11] + y_array[14] + y_array[15] + y_array[20])/ total_sum, 
+                        (y_array[0] + y_array[5] + y_array[6] + y_array[7] + y_array[8] + y_array[12] + y_array[16] + y_array[17] + y_array[22])/ total_sum, 
+                        (y_array[9] + y_array[10] + y_array[13] + y_array[18] + y_array[19] + y_array[24])  / total_sum,
+                        (y_array[31] + y_array[36] + y_array[37] + y_array[38] + y_array[39] + y_array[48] + y_array[51] + y_array[52] + y_array[57])/ total_sum, 
+
+                        (y_array[33] + y_array[40] + y_array[41] + y_array[42] + y_array[43] + y_array[49] + y_array[53] + y_array[54] + y_array[59])/ total_sum, 
+
+                        (y_array[35] + y_array[44] + y_array[45] + y_array[46] + y_array[47] + y_array[50] + y_array[55] + y_array[56] + y_array[61])/ total_sum, 
+                        (y_array[68] + y_array[73] + y_array[74] + y_array[75] + y_array[83] + y_array[86])/ total_sum, 
+                        (y_array[70] + y_array[76] + y_array[77] + y_array[78] + y_array[79] + y_array[84] + y_array[87] + y_array[88] + y_array[91])/ total_sum, 
+                        (y_array[72] + y_array[80] + y_array[81] + y_array[82] + y_array[85] + y_array[89])/ total_sum, 
+                        ))
+                else:
+                    Passes = np.array([4, 4,3,2,2,2, 2, 4,3,2,2,2, 4,3,2,2,2,2,1,2,1,2,1,1,
+                                       2,1,1,1,1, 2, 2,1,1,1,1, 1])
+
+                    Contributions = np.array([0.02575, 0.02575,0.0406,0.0406,0.01435,0.02575,
+                                              0.02575, 0.02575,0.0406,0.01435,0.0406,0.02575,
+                                              0.02575,0.0406,0.01435,0.0406,0.02575,0.01435,0.01435,0.0406,0.0406,0.02575,0.01435,0.02575,
+                                              0.02575,0.0406,0.01435,0.0406,0.02575, 0.02575,
+                                              0.02575,0.0406,0.0406,0.01435,0.02575, 0.02575])
+
+                    Contributions = Contributions / Contributions.sum()
+                    I0 = np.max(layer_data)
+                    Passes = I0 - I0/(1+ np.exp(-(Passes - x0)))
+                    y_array = Passes*Contributions
+                    total_sum = np.sum(y_array)
+
+                    result = list((
+                            (y_array[0])/ total_sum, 
+                            (np.sum(y_array[1:6]))/ total_sum, 
+                            (y_array[6])/ total_sum,
+                            (np.sum(y_array[7:12])) / total_sum,
+                            (np.sum(y_array[12:24])) / total_sum,
+                            (np.sum(y_array[24:29])) / total_sum,
+                            (y_array[29])/ total_sum,
+                            (np.sum(y_array[30:35])) / total_sum,
+                            (y_array[35])/ total_sum
+                            ))
+
+                kernel = np.array(result / np.sum(result))
+                kernel = kernel.reshape(3, 3)
+                plt.imshow(kernel, cmap="gray", interpolation="nearest")
+
+                # Normalize the data
+                layer_data = (layer_data - layer_data.min()) / (layer_data.max() - layer_data.min())
+                  
+                layer_data_denoise = layer_data
+                #Clip values to remove 0s (handled badly by RLD)
+                layer_data_denoise = np.clip(layer_data_denoise, 1e-4, None)
+                
+                # Richardson-Lucy deconvolution
+                deconvolved_image = richardson_lucy(layer_data_denoise, kernel, iterations)
+                
+                # Convert to 16-bit
+                deconvolved_image_uint16 = img_as_uint(deconvolved_image)
+                processed_layers.append(deconvolved_image_uint16)
+                original_layers.append(layer_data_denoise)
+            # Stack all processed layers into a single multi-layer TIFF
+            processed_tiff = np.stack(processed_layers, axis=0)
+            scan_data = np.stack(scan_data, axis=0)
+            
+            #Removing a 2 pixel (1um) border to account for border effect (remove if necessary)
+            processed_tiff_cropped = processed_tiff[:, 3:-3, 3:-3]
+            scan_data = scan_data[:, 3:-3, 3:-3]
+
+            
+            # Save the processed TIFF to the output directory
+            output_path = os.path.join(output_dir, filename)
+            imwrite(output_path, processed_tiff_cropped)
+            
+            name, ext = os.path.splitext(filename)
+            new_filename = f"{name}_new{ext}"
+            output_path = os.path.join(input_dir, new_filename)
+            #imwrite(output_path, scan_data) Can choose to add in a normalized image for quick comparison but not necessary
+    
+    print(f"Processing complete. Files saved in {output_dir}")
+
+# %%
+input_dir = "/PATH/TO/INPUT/DIR/"
+output_dir = "/PATH/TO/OUTPUT/DIR/"
+
+# %%
+#HR-IMC (333 nm) 
+RLD_HRIMC_circle(input_dir, output_dir, x0 = 8, resolution = 333) #resolution = 500 for 500nm HR-IMC. Adjust x0 as described. 
+
+
